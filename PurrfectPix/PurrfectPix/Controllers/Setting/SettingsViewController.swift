c2db82a6e5f5a2fc6bdb349d3a49318357619d3d
Amber Liu <sapphireinruby@gmail.com>
2021-11-25 05:20:09 +0800

    Create privacy,EULA and delete account at setting under profile

--- PurrfectPix/PurrfectPix/Controllers/Setting/SettingsViewController.swift
+++ PurrfectPix/PurrfectPix/Controllers/Setting/SettingsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -16,11 +17,14 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         return table
     }()
 
+    private var sections: [SettingsSection] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Settings"
          // tableView
         view.addSubview(tableView)
+        configureModels()
         tableView.delegate = self
         tableView.dataSource = self
         view.backgroundColor = .systemBackground
@@ -32,8 +36,8 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         )
         createTableFooter()
 
-
     }
+
 
     @objc func didTapClose() {
         dismiss(animated: true, completion: nil)
@@ -42,6 +46,71 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
+    }
+
+    private func configureModels() {
+        sections.append(
+            SettingsSection(title: "We Hope you Enjoy PurrfectPix", options: [
+                SettingOption(
+                    title: "Rate PurrfectPix",
+                    image: UIImage(systemName: "star"),
+                    color: .systemGray2
+                ) {
+
+                },
+                SettingOption(
+                    title: "Share PurrfectPix",
+                    image: UIImage(systemName: "square.and.arrow.up"),
+                    color: .systemGray2
+                ) {
+
+                }
+            ])
+        )
+
+        sections.append(
+            SettingsSection(title: "Information", options: [
+                SettingOption(
+                    title: "End User License Agreement",
+                    image: UIImage(systemName: "doc"),
+                    color: .P2!
+                ) { [weak self] in
+                    DispatchQueue.main.async {
+                        guard let url = URL(string: "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/") else {
+                            return
+                        }
+                        let vcSF = SFSafariViewController(url: url)
+                        self?.present(vcSF, animated: true, completion: nil)
+                    }
+                },
+                SettingOption(
+                    title: "Privacy Policy",
+                    image: UIImage(systemName: "hand.raised"),
+                    color: .P1!
+                ) { [weak self] in
+                    guard let url = URL(string: "https://www.privacypolicies.com/live/dd1fde8e-ef94-48a1-8b08-49b95c29ac5e") else {
+                        return
+                    }
+                    let vc = SFSafariViewController(url: url)
+                    self?.present(vc, animated: true, completion: nil)
+
+                },
+                SettingOption(
+                    title: "Contact PurrfectPix",
+                    image: UIImage(systemName: "message"),
+                    color: .P2!
+                ) {
+
+                },
+                SettingOption(
+                    title: "Delete Account",
+                    image: UIImage(systemName: "minus.circle"),
+                    color: .systemPink
+                ) {
+
+                }
+            ])
+        )
     }
 
     // Table
@@ -82,13 +151,33 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         present(actionSheet, animated: true)
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return sections[section].options.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        return UITableViewCell()
+        let model = sections[indexPath.section].options[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.title
+        cell.imageView?.image = model.image
+        cell.imageView?.tintColor = model.color
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = sections[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
     }
 
 }
